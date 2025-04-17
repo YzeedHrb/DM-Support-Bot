e47ea359e3bd47254fe47e46c3171f3a7581d6e9
يزيد الحربي <138837570+YzeedHrb@users.noreply.github.com>
2025-04-18 00:08:59 +0300

    Update utils.py
    Translate comments

--- utils.py
+++ utils.py
@@ -1,13 +1,3 @@
-"""
-Discord Support Bot Utilities
-Developed by YzeedHrb
-GitHub: https://github.com/YzeedHrb
-Discord: YzeedHrb#0000
-
-This utilities file is part of the Discord Support Bot.
-Any unauthorized distribution or modification of this code is strictly prohibited.
-"""
-
 import json
 import os
 from datetime import datetime
